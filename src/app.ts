95e8c254c7f7c264649e78d09c923f41f9c67f0b
Touhidul Shawan <touhidulshawan@gmail.com>
2021-06-14 20:18:59 +0600

    add a logger

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { connection } from "./db/connect";
+import log from "./logger";
 
 const port = process.env.PORT || 3000;
 
@@ -9,7 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.listen(port, () => {
-  console.log(`App is running on port ${port}`);
+  log.info(`App is running on port ${port}`);
   connection()
     .then(() => console.log("connected to database"))
     .catch((error) => console.log("failed to connect" + error));
